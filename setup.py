6e2a8b1ee9476b8f717ff5a60d331981f21a00f5
Mark Palmeri <mlp6@duke.edu>
2018-01-06 17:19:10 -0500

    add py version of setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+try:
+    os.chdir(os.environ.get('HOME'))
+except:
+   sys.exit()
+
+dotfiles = ['.alias',
+            '.bashrc',
+            '.bash_profile',
+            '.gitconfig',
+            '.gitignore_global',
+            '.tmux.conf',
+            '.vimrc',
+            '.zshrc']
+
+for dotfile in dotfiles:
+    if os.path.exists(dotfile):
+        os.system('mv {} {}.save', (dotfile, dotfile))
+    os.system('ln -s ~/.config/dotfiles/{}', (dotfile))
+
+os.system('git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.config/oh-my-zsh')
+
+os.system('git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim')
+os.system('vim +PluginInstall +qall')
